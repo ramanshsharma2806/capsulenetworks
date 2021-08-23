8d05505cb9b8eca1b0493560c332e52497abee99
Ramansh Sharma <sharmar@bxscience.edu>
2021-08-24 00:42:39 +0530

    added notebook

--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class PrimaryCaps(nn.Module):
+class Capsule(nn.Module):
     def __init__(self):
-        super(PrimaryCaps, self).__init__()
+        super(Capsule, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=32, out_channels=8, kernel_size=9, stride=2, padding=0)
 
     def forward(self, x):
