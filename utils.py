48c5e51839a089cb29e48633d19e5f505197397b
Ramansh Sharma <sharmar@bxscience.edu>
2021-08-24 23:25:13 +0530

    we do not need a generic capsules object

--- utils.py
+++ utils.py
@@ -3,16 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Capsule(nn.Module):
-    def __init__(self):
-        super(Capsule, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=32, out_channels=8, kernel_size=9, stride=2, padding=0)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        return x
-
-
 class PrimaryCaps(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, caps_dims=8):
         """
